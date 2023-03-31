25f9dbac1428ab1dde5dda5905055c7491feab65
Serenata2 <astg98@gmail.com>
2023-03-31 14:54:02 +0900

    project 경로 수정

--- IndoorNavigationApp/sdk/build/intermediates/cxx/Debug/71b1y493/logs/x86_64/configure_command.bat
+++ IndoorNavigationApp/sdk/build/intermediates/cxx/Debug/71b1y493/logs/x86_64/configure_command.bat
@@ -1,6 +1,6 @@
 @echo off
 "C:\\Users\\HyukSu\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HC:\\Users\\HyukSu\\Desktop\\IndoorNavigationApp\\sdk\\libcxx_helper" ^
+  "-HC:\\Users\\HyukSu\\Desktop\\Indoor-Navigation-App\\IndoorNavigationApp\\sdk\\libcxx_helper" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=21" ^
@@ -11,9 +11,9 @@
   "-DCMAKE_ANDROID_NDK=C:\\Users\\HyukSu\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
   "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\HyukSu\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=C:\\Users\\HyukSu\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\HyukSu\\Desktop\\IndoorNavigationApp\\sdk\\build\\intermediates\\cxx\\Debug\\71b1y493\\obj\\x86_64" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\HyukSu\\Desktop\\IndoorNavigationApp\\sdk\\build\\intermediates\\cxx\\Debug\\71b1y493\\obj\\x86_64" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\HyukSu\\Desktop\\Indoor-Navigation-App\\IndoorNavigationApp\\sdk\\build\\intermediates\\cxx\\Debug\\71b1y493\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\HyukSu\\Desktop\\Indoor-Navigation-App\\IndoorNavigationApp\\sdk\\build\\intermediates\\cxx\\Debug\\71b1y493\\obj\\x86_64" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BC:\\Users\\HyukSu\\Desktop\\IndoorNavigationApp\\sdk\\.cxx\\Debug\\71b1y493\\x86_64" ^
+  "-BC:\\Users\\HyukSu\\Desktop\\Indoor-Navigation-App\\IndoorNavigationApp\\sdk\\.cxx\\Debug\\71b1y493\\x86_64" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
