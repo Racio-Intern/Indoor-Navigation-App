bddbf7db51cd54ab39f66ffa650e81b7890edabd
hhtboy <hhtboy1024@gmail.com>
2023-03-31 17:28:01 +0900

    ApriltagCamera2View, CameraBridgeViewBaseImpl 추가

--- IndoorNavigationApp/sdk/build/intermediates/cxx/Debug/71b1y493/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\HyukSu\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\HyukSu\\Desktop\\Indoor-Navigation-App\\IndoorNavigationApp\\sdk\\.cxx\\Debug\\71b1y493\\x86_64" ^
-  opencv_jni_shared
