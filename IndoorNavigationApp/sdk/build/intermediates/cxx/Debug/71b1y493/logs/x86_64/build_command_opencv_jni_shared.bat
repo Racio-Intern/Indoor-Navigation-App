aaec42538477f9eef7e4b89c365ec4f9e9e09eaa
Serenata2 <astg98@gmail.com>
2023-03-31 14:01:09 +0900

    first commit

---
+++ IndoorNavigationApp/sdk/build/intermediates/cxx/Debug/71b1y493/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\HyukSu\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\HyukSu\\Desktop\\IndoorNavigationApp\\sdk\\.cxx\\Debug\\71b1y493\\x86_64" ^
+  opencv_jni_shared
