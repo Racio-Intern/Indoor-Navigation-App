554997860244be3c8cf1bf2cc3a19f6d40fc5f78
hhtboy <hhtboy1024@gmail.com>
2023-04-07 11:34:56 +0900

    최신버전 안드로이드에서 실행되지 않는 버그 수정

--- IndoorNavigationApp/app/src/main/java/com/example/indoornavigationapp/MainActivity.kt
+++ IndoorNavigationApp/app/src/main/java/com/example/indoornavigationapp/MainActivity.kt
@@ -6,20 +6,27 @@ import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.TextView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import apriltag.ApriltagNative
 import com.example.indoornavigationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val LOGTAG = "MainActivity"
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+        setupActionBarWithNavController(navController)
+
+        setContentView(binding.root)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
         } else {
+            @Suppress("DEPRECATION")
             window.setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
@@ -33,7 +40,6 @@ class MainActivity : AppCompatActivity() {
         ApriltagNative.apriltag_native_init()
         ApriltagNative.apriltag_init("tagStandard41h12", 2, 4.0, 0.0, 1)
 
-        setContentView(binding.root)
     }
 
     companion object {
@@ -43,5 +49,10 @@ class MainActivity : AppCompatActivity() {
             System.loadLibrary("opencv_native_lib")
             System.loadLibrary("apriltag_native_lib")
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
